b96a89ba46baf358a69dbb555f949ac46fe83705
cesar Tello <aquinocesarignacio@gmail.com>
2024-01-29 18:38:22 -0300

    EndActivi4

---
+++ src/components/Carrito.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Carrito = () => {
+  return (
+    <div className='container mx-auto'>
+      <h2>Carrito</h2>
+    </div>
+  )
+}
+
+export default Carrito
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -22,9 +22,9 @@ const NavBar = () => {
                                 <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0m4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4m-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664z" />
                             </svg>
                         </li>
-                        <li className='flex'>
+                        <Link to={"/Carrito"} className='flex'>
                             <CardWidget />
-                        </li>
+                        </Link>
                         <li className='flex'>
                             <div className='flex justify-center rounded-[0.2rem] bg-slate-200 sm:hidden '>
                                 <Menu/>
